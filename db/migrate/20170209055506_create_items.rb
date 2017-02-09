2dff916a1dcd69376f76621762dbb34ffe9076f6
warrend <warrend555@gmail.com>
2017-02-09 01:06:04 -0500

    creates tables

--- db/migrate/20170209055506_create_items.rb
+++ db/migrate/20170209055506_create_items.rb
@@ -1,4 +1,11 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
+    create_table :items do |t|
+      t.string :name 
+      t.string :location
+      t.text :description
+      t.integer :tag_id
+      t.integer :user_id
+    end 
   end
 end

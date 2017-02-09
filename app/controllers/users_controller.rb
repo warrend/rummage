3c5ea932f6ca71e24688f6fc2f676f445afc4e46
warrend <warrend555@gmail.com>
2017-02-09 17:18:42 -0500

    add user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,30 @@
 class UsersController < ApplicationController
 
+  get '/users/:slug' do 
+
+  end 
+
+  get '/signup/?' do 
+    if !logged_in?
+      erb :'users/create'
+    else
+      erb :'items/index'
+    end
+  end
+
+  post '/signup' do 
+
+  end
+
+  get '/login/?' do 
+
+  end
+
+  post '/login' do 
+
+  end
+
+  get '/logout/?' do 
+
+  end
 end

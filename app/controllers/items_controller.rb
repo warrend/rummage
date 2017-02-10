25633ede689eb08400859016ddc9487a2383372b
warrend <warrend555@gmail.com>
2017-02-10 00:29:14 -0500

    add signup/login forms

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,11 @@
 class ItemsController < ApplicationController
 
+  get '/items' do 
+    if logged_in?
+      @user = User.find_by_id(session[:user_id])
+      erb :'items/index'
+    else
+      redirect '/signup'
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   end 
 
-  get '/signup/?' do 
+  get '/signup' do 
     if !logged_in?
       erb :'users/create'
     else
@@ -13,18 +13,28 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do 
-
+    if params[:username] == '' || params[:email] == '' || params[:password] == ''
+      redirect '/signup'
+    else
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+      redirect '/items'
+    end
   end
 
-  get '/login/?' do 
-
+  get '/login' do 
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect :'/items'
+    end
   end
 
   post '/login' do 
 
   end
 
-  get '/logout/?' do 
+  get '/logout' do 
 
   end
 end
